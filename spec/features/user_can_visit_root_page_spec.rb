a03ecd29ec958ea54062b3453fb26675724fc2fc
Dione Wilson <dionew1@gmail.com>
2017-12-13 09:28:00 -0700

    Setup test and begin in views

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -8,4 +8,30 @@ feature "User can visit root page" do
       expect(page).to have_selector("input[value='Search by zip...']")
     end
   end
+
+  scenario "and get search results" do
+    visit "/"
+
+    fill_in "search", with: 80203
+    click_on "locate"
+
+    expect(current_path).to eq "/search"
+    expect(stations.count).to eq 10
+    expect(page).to have_selector "name"
+    expect(page).to have_selector "address"
+    expect(page).to have_selector "fuel_types"
+    expect(page).to have_selector "distance"
+    expect(page).to have_selector "access_times"
+  end
 end
+
+
+
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80203
+# And I click "Locate"
+# Then I should be on page "/search" with parameters visible in the url
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
